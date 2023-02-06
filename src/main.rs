04f637324d8cc5e9eb896240e2e3a61111fe08e7
DrEden33773 <edwardwang33773@gmail.com>
2023-02-07 00:27:53 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use rand::Rng;
+
 mod sort;
 mod tool;
 
+#[allow(unused_variables)]
 fn main() {
+    let mut rng = rand::thread_rng();
+
     let vec: Vec<usize> = vec![4, 3, 8, 45, 3, 7, 98, 1, 0];
-    sort::dbg_all_sorts(&vec);
+
+    let to_sort: Vec<usize> = (0..50000)
+        .map(|_| rng.gen_range(0..i32::MAX as usize))
+        .collect();
+
+    sort::benchmark_all_sorts(&to_sort);
 }
--- src/sort.rs
+++ src/sort.rs
@@ -18,7 +18,9 @@ use shell_sort::shell_sort;
 
 use crate::tool::println_vec;
 use std::fmt::Display;
+use std::time;
 
+#[allow(dead_code)]
 pub fn dbg_non_radix_sorts<T>(vec: &Vec<T>)
 where
     T: Display + PartialOrd + Copy,
@@ -48,6 +50,7 @@ where
     }
 }
 
+#[allow(dead_code)]
 pub fn dbg_all_sorts(vec: &Vec<usize>) {
     /* debug non_radix sorts at first */
     dbg_non_radix_sorts(&vec);
@@ -65,5 +68,40 @@ pub fn dbg_all_sorts(vec: &Vec<usize>) {
         println_vec(&mut to_sort);
         // println
         println!();
+    }
+}
+
+#[allow(dead_code)]
+pub fn benchmark_all_sorts(vec: &Vec<usize>) {
+    println!();
+    let sort_func_table: Vec<(&str, fn(&mut Vec<usize>))> = vec![
+        ("quick_sort", quick_sort),
+        ("heap_sort", heap_sort),
+        ("merge_sort", merge_sort),
+        ("radix_sort", radix_sort),
+        ("shell_sort", shell_sort),
+        ("selection_sort", selection_sort),
+        ("insertion_sort", insertion_sort),
+        ("bubble_sort", bubble_sort),
+    ];
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(8)
+        .build()
+        .unwrap();
+    for (sort_name, sort_func) in sort_func_table {
+        {
+            // get clone
+            let mut to_sort = vec.clone();
+            // start time
+            let start_time = time::Instant::now();
+            // exec sort
+            sort_func(&mut to_sort);
+            // end time
+            let end_time = time::Instant::now();
+            // get duration
+            let duration = end_time - start_time;
+            // println
+            println!("{} => {} seconds.\n", sort_name, duration.as_secs_f32());
+        };
     }
 }
