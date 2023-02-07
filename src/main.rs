8e1eb348c79b983865990cd7c17e5cf8c3da060f
DrEden33773 <edwardwang33773@gmail.com>
2023-02-07 20:58:07 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 mod sort;
 
-#[allow(unused_variables)]
-fn main() {
+#[allow(dead_code)]
+fn dbg() {
     use rand::Rng;
-
-    let to_sort: Vec<usize> = (0..50000)
-        .map(|_| rand::thread_rng().gen_range(0..i32::MAX as usize))
-        .collect();
-
     let simple: Vec<usize> = (0..15)
         .map(|_| rand::thread_rng().gen_range(0..100))
         .collect();
+    sort::debug_all_sorts(&simple);
+}
 
-    // sort::debug_all_sorts(&simple);
-    sort::benchmark_all_sorts(&to_sort);
+#[allow(dead_code)]
+fn benchmark(times: usize) {
+    use rand::Rng;
+    println!();
+    for sample in 0..times {
+        println!("sample {}", sample);
+        let to_sort: Vec<usize> = (0..50000)
+            .map(|_| rand::thread_rng().gen_range(0..i32::MAX as usize))
+            .collect();
+        sort::benchmark_all_sorts(&to_sort);
+    }
+}
+
+#[allow(unused_variables)]
+fn main() {
+    // dbg();
+    benchmark(10);
 }
